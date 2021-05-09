942714e0d7980f77d1993b45f150f0d9670477b4
Partydragen <admin@partydragen.com>
2021-05-09 18:47:01 +0200

    Fix install issue

--- upload/modules/MCStatistics/init.php
+++ upload/modules/MCStatistics/init.php
@@ -17,7 +17,11 @@ require_once(ROOT_PATH . '/modules/MCStatistics/module.php');
 $module = new MCStatistics_Module($language, $mcstatistics_language, $pages, $queries, $navigation, $cache);
 
 // Profile page tab
-if($configuration->get('mcstatistics', 'display_profile') == 1) {
-    if (!isset($profile_tabs)) $profile_tabs = array();
-    $profile_tabs['mcstatistics'] = array('title' => $mcstatistics_language->get('mcstatistics', 'ingame'), 'smarty_template' => 'mcstatistics/profile_tab.tpl', 'require' => ROOT_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . 'MCStatistics' . DIRECTORY_SEPARATOR . 'profile_tab.php');
+try {
+    if($configuration->get('mcstatistics', 'display_profile') == 1) {
+        if (!isset($profile_tabs)) $profile_tabs = array();
+        $profile_tabs['mcstatistics'] = array('title' => $mcstatistics_language->get('mcstatistics', 'ingame'), 'smarty_template' => 'mcstatistics/profile_tab.tpl', 'require' => ROOT_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . 'MCStatistics' . DIRECTORY_SEPARATOR . 'profile_tab.php');
+    }
+} catch(Exception $e){
+    // Error
 }
