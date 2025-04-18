f5cf512fa331ce2a545c139377ff941d6bb0c8fa
partydragen <admin@partydragen.com>
2025-04-18 16:32:24 +0200

    Release v1.3.2

--- upload/modules/MCStatistics/module.php
+++ upload/modules/MCStatistics/module.php
@@ -19,7 +19,7 @@ class MCStatistics_Module extends Module {
 
         $name = 'MCStatistics';
         $author = '<a href="https://partydragen.com" target="_blank" rel="nofollow noopener">Partydragen</a> and my <a href="https://partydragen.com/supporters/" target="_blank">Sponsors</a>';
-        $module_version = '1.3.2';
+        $module_version = '1.3.3';
         $nameless_version = '2.2.0';
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
